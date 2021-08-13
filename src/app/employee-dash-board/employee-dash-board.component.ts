89f3be2335d0ac4547ca36554ba565e43a8fac3b
varshan-kb <88645707+varshan-kb@users.noreply.github.com>
2021-08-13 15:27:12 +0530

    init

--- src/app/employee-dash-board/employee-dash-board.component.ts
+++ src/app/employee-dash-board/employee-dash-board.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit ,ErrorHandler} from '@angular/core';
-import {FormBuilder,FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { EmployeeModel } from './employee-dash board.model';
-import {ApiService} from '../shared/api.service';
+import { ApiService } from '../shared/api.service';
 import { Message } from '@angular/compiler/src/i18n/i18n_ast';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ViewEncapsulation } from '@angular/compiler/src/core';
+
 
 @Component({
   selector: 'app-employee-dash-board',
@@ -13,72 +12,90 @@ import { ViewEncapsulation } from '@angular/compiler/src/core';
 })
 export class EmployeeDashBoardComponent implements OnInit {
 
-  formValue !: FormGroup;
+  /* formValue !: FormGroup; */
   //Currentbranchid:any;
-  searchenddate:any='';
-  employeeModelobj : EmployeeModel = new EmployeeModel();
- // api: any;
+  searchenddate: any = '';
+  employeeModelobj: EmployeeModel = new EmployeeModel();
+  // api: any;
   employeeData: any;
-  constructor(private formbuilder: FormBuilder, private api:ApiService) { }
+  constructor(private formbuilder: FormBuilder, private api: ApiService) { }
 
-  form = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
+  formValue = new FormGroup({
+    //id: new FormControl(''),
+    firstname: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern('^[a-zA-Z ]*$')]),
+    lastname: new FormControl('', [Validators.required/* , Validators.minLength(2), Validators.maxLength(10), Validators.pattern('^[a-zA-Z ]*$') */]),
     emailid: new FormControl('', [
       Validators.required,
-      Validators.email
+      Validators.minLength(5),
+      Validators.maxLength(80),
+      Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
     ]),
-    mobileno: new FormControl('', [
-      Validators.required,
-      Validators.minLength(10)
-    ])
-   });
+    mobileNumber: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+    salary: new FormControl(''),
+
+    /* form: FormGroup = new FormGroup({});  
+    constructor(private fb: FormBuilder) {  
+     this.form = fb.group({  
+       mobileNumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]]  
+     })  
+   }   */
+
+  });
   ngOnInit(): void {
-    this.formValue = this.formbuilder.group({
+    
+    /* this.formValue = this.formbuilder.group({
       id: '',
       firstname: [' '],
       lastname: [' '],
       emailid: [' '],
       mobileno: '',
       salary: '',
-   })
-   this.getAllEmployee();
+   }) */
+    this.getAllEmployee();
+  }
+  get firstname(){
+    return this.formValue.get('firstname')
+  }
+  get lastname(){
+    return this.formValue.get('firstname')
+  }
+  get mobileNumber(){
+    return this.formValue.get('mobileNumber')
   }
   //branch_name_change(branchid:any) {
-    //this.Currentbranchid = branchid;
+  //this.Currentbranchid = branchid;
   //}
-  postEmployeeDetails(){
-    this.employeeModelobj.id = this.formValue.value.id;
+  postEmployeeDetails() {
+    //this.employeeModelobj.id = this.formValue.value.id;
     this.employeeModelobj.firstname = this.formValue.value.firstname;
     this.employeeModelobj.lastname = this.formValue.value.lastname;
     this.employeeModelobj.emailid = this.formValue.value.emailid;
-    this.employeeModelobj.mobilenumber = this.formValue.value.mobileno;
+    this.employeeModelobj.mobilenumber = this.formValue.value.mobileNumber;
     this.employeeModelobj.salary = this.formValue.value.salary;
     this.api.postEmployee(this.employeeModelobj)
-     .subscribe((response: any)=>{
-      console.log(response);
-      alert("Employee Added Successfully") 
-      this.formValue.reset();
-      let ref=document.getElementById('cancel');
-      ref?.click();
-      this.getAllEmployee();
-    },
+      .subscribe((response: any) => {
+        console.log(response);
+        alert("Employee Added Successfully")
+        this.formValue.reset();
+        let ref = document.getElementById('cancel');
+        ref?.click();
+        this.getAllEmployee();
+      },
 
-      (error)=>{
-      alert ("Something went wrong");
-    });
+        (error) => {
+          alert("Something went wrong");
+        });
   }
 
-  getAllEmployee(){
- 
+  getAllEmployee() {
+
     try {
       this.api.getEmployee()
         .subscribe((response: any) => {
           this.employeeData = response;
         });
     }
-    catch (Exception)
-    {
+    catch (Exception) {
       throw new Exception(Message);
     }
 
@@ -95,40 +112,39 @@ export class EmployeeDashBoardComponent implements OnInit {
           this.getAllEmployee();
         }
       });*/
-      if (confirm("Are you sure to delete " + this.formValue.value.firstname)) {
-        this.api.deleteEmployees(row.id)
+    if (confirm("Are you sure to delete " + this.formValue.value.firstname)) {
+      this.api.deleteEmployees(row.id)
         .subscribe(response => {
           alert("Employee Data Deleted");
           this.getAllEmployee();
         });
-      }
-      else{
-        this.getAllEmployee();
-      }
+    }
+    else {
+      this.getAllEmployee();
+    }
   }
-  onEdit(row: any){
+  onEdit(row: any) {
 
     //this.showAdd = false;
     //this.showUpdate = true;
 
- this.employeeModelobj.id = row.id;
- this.formValue.controls['id'].setValue (row.id);
- this.formValue.controls['firstname'].setValue (row.firstname);
- this.formValue.controls['lastname'].setValue (row.lastname);
- this.formValue.controls['emailid'].setValue (row.emailid);
- this.formValue.controls['mobileno'].setValue (row.mobilenumber);
- this.formValue.controls['salary'].setValue (row.salary);
-}
+    this.employeeModelobj.id = row.id;
+    //this.formValue.controls['id'].setValue(row.id);
+    this.formValue.controls['firstname'].setValue(row.firstname);
+    this.formValue.controls['lastname'].setValue(row.lastname);
+    this.formValue.controls['emailid'].setValue(row.emailid);
+    this.formValue.controls['mobileNumber'].setValue(row.mobilenumber);
+    this.formValue.controls['salary'].setValue(row.salary);
+  }
 
-resetEmployeeDetails(){
+  resetEmployeeDetails() {
 
-this.formValue.reset();
+    this.formValue.reset();
 
-}
-updateEmployeeDetails()
-{
+  }
+  updateEmployeeDetails() {
 
-    this.employeeModelobj.id = this.formValue.value.id;
+    //this.employeeModelobj.id = this.formValue.value.id;
     this.employeeModelobj.firstname = this.formValue.value.firstname;
     this.employeeModelobj.lastname = this.formValue.value.lastname;
     this.employeeModelobj.emailid = this.formValue.value.emailid;
@@ -137,7 +153,7 @@ updateEmployeeDetails()
 
     this.api.updateEmployee(this.employeeModelobj, this.employeeModelobj.id)
 
-      .subscribe((response: any)=>{
+      .subscribe((response: any) => {
         console.log(response);
         alert("Updated Successfully")
         this.formValue.reset();
@@ -147,5 +163,5 @@ updateEmployeeDetails()
       });
 
   }
-  
+
 }
